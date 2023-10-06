655ab08330399bbfad825125843dae7ec6460b8b
eniss21 <eniskarriqi99@gmail.com>
2023-10-06 23:55:28 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7ed47f2e-75c7-4902-9e27-45569ec4e246';
+export const MODAL_CLOSE_SEC = 2.5;
